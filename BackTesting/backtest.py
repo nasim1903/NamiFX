a9e950a21368f40e6550691210e688fe595a817a
Nasim Ahmed <Nasim.Ahmed@melroseinteriors.co.uk>
2025-03-05 09:26:25 +0000

    Testing backtest params

--- BackTesting/backtest.py
+++ BackTesting/backtest.py
@@ -14,12 +14,10 @@ from Strategies.MeanReversion import MeanReversionStrategy
 from Strategies.SupplyAndDemand import TrendFollowingStrategy
 from Strategies.CrashAndBoom import CrashBoomStrategy
 
-
-
 class Backtester:
 
     @staticmethod
-    def runBackTestForStrategy(strategy: Type[bt.Strategy], plot: bool = True, fxdata: dl.Data = dl.Data(timeframe=mt5.TIMEFRAME_M15, numOfCandles=1000, symbol='EURUSD')):
+    def runBackTestForStrategy(strategy: Type[bt.Strategy], plot: bool = True, fxdata: dl.Data = dl.Data(timeframe=mt5.TIMEFRAME_H1, numOfCandles=1000, symbol='EURUSD')):
         """
         Runs a backtest using the provided strategy.
 
@@ -27,7 +25,6 @@ class Backtester:
         :param plot: Boolean to determine whether to plot the results. Will only run 1 instance of the strategy
         """
         cerebro = bt.Cerebro()
-
 
         # Feed data into Backtrader
         btData = bt.feeds.PandasData(dataname=fxdata.full_data)
@@ -40,49 +37,50 @@ class Backtester:
 
         # Set initial cash
         cerebro.broker.setcash(100000)
-
         cerebro.broker.setcommission(0.01)
 
         # Run optimization with multiprocessing
         cerebro.run()
 
-
         # Plot results if needed
         if plot:
             cerebro.plot(style='bar')
-            
 
-    def runAllBacktest(fxdata: dl.Data = dl.Data(timeframe=mt5.TIMEFRAME_M15, numOfCandles=1000, symbol='EURUSD')):
+    def runAllBacktest():
 
         cerebro = bt.Cerebro()
         # Feed data into Backtrader
         
-        btData = bt.feeds.PandasData(dataname=fxdata.full_data)
-        cerebro.adddata(btData)
+        btData15m = bt.feeds.PandasData(dataname=dl.Data(timeframe=mt5.TIMEFRAME_M15, symbol='EURUSD').get_last_2_weeks_data())
+        btData1h = bt.feeds.PandasData(dataname=dl.Data(timeframe=mt5.TIMEFRAME_H1, symbol='EURUSD').get_last_2_weeks_data())
+        
+        cerebro.adddata(btData15m)
+        cerebro.adddata(btData1h)
 
         # Add a FixedSize sizer according to the stake
         cerebro.addsizer(bt.sizers.FixedSize, stake=10000)
-        
-        # Add a strategy
+
         cerebro.optstrategy(
-            MeanReversionStrategy,
-            bollinger_period=range(10, 20),
-            atr_period = range(10,20),
-            atr_mult=range(2,10),
-            profit_mult=range(1,10)
+            MaCrossOverBt,
+            maperiod=range(10, 13),
             )
 
-        cerebro.addstrategy(MeanReversionStrategy)
+        # # Add a strategy
+        cerebro.optstrategy(
+            MeanReversionStrategy,
+            bollinger_period=range(10, 11),
+            atr_period = range(10,11),
+            atr_mult=range(2,3),
+            profit_mult=range(1,2)
+            )
 
         # Set initial cash
         cerebro.broker.setcash(100000)
 
         cerebro.broker.setcommission(0.01)
 
-
-
         # Run optimization with multiprocessing
-        cerebro.run(maxcpus=12)
+        cerebro.run()
         
         # # Flatten the results list
         # results = [strategy for sublist in results for strategy in sublist]
@@ -94,9 +92,11 @@ class Backtester:
         # print(f"Best MA Period: {best_strategy.params.maperiod} with Final Value: {best_strategy.broker.get_value():.2f}")
     
 
-
 if __name__ == '__main__':
     # Required for Windows to properly handle multiprocessing
     multiprocessing.freeze_support()  
+
+    Backtester.runAllBacktest()
+
     # Run the backtest using TestStrategy
-    Backtester.runBackTestForStrategy(TrendFollowingStrategy, plot=True)
+    # Backtester.runBackTestForStrategy(CrashBoomStrategy, plot=True)
